525a4ab33a4d542eda2b2637102eee87421bcadf
Barathkannan2005 <barath.s2023cse@sece.ac.in>
2025-01-31 09:41:06 +0530

    index.js modified

--- Backend/index.js
+++ Backend/index.js
@@ -86,6 +86,6 @@ app.get('/', async (req, res) => {
 });
 
 // Start server
-app.listen(3000, () => {
-    console.log("App is running on port 3000");
+app.listen(5004, () => {
+    console.log("App is running ");
 });
